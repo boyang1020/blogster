3da3e42230e37900046531829f89a07eca35a465
boyang1020 <boyang1020@gmail.com>
2019-07-06 15:49:25 -0700

    clear hash

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
+const { clearHash } = require('../services/cache');
 
 const Blog = mongoose.model('Blog');
 
@@ -56,5 +57,6 @@ module.exports = app => {
 		} catch (err) {
 			res.send(400, err);
 		}
+		clearHash(req.user.id);
 	});
 };
--- services/cache.js
+++ services/cache.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const redis = require('redis');
 const util = require('util');
+// const keys = require('../config/keys');
 
 const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
@@ -25,7 +26,7 @@ mongoose.Query.prototype.exec = async function() {
 			collection: this.mongooseCollection.name,
 		})
 	);
-	const cacheValue = await client.hget(this.kahsKey, key);
+	const cacheValue = await client.hget(this.hashKey, key);
 
 	if (cacheValue) {
 		const doc = JSON.parse(cacheValue);
@@ -35,4 +36,10 @@ mongoose.Query.prototype.exec = async function() {
 	const result = await exec.apply(this, arguments);
 	client.hset(this.hashkey, key, JSON.stringify(result), 'EX', 10);
 	return result;
+};
+
+module.exports = {
+	clearHash(hashKey) {
+		client.del(JSON.stringify(hashKey));
+	},
 };
