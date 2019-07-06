df79e4a3fc4198435fc2ee17d0360f71e927d6ef
boyang1020 <boyang1020@gmail.com>
2019-07-06 15:21:12 -0700

    nested hashes

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -4,55 +4,57 @@ const requireLogin = require('../middlewares/requireLogin');
 const Blog = mongoose.model('Blog');
 
 module.exports = app => {
-  app.get('/api/blogs/:id', requireLogin, async (req, res) => {
-    const blog = await Blog.findOne({
-      _user: req.user.id,
-      _id: req.params.id
-    });
+	app.get('/api/blogs/:id', requireLogin, async (req, res) => {
+		const blog = await Blog.findOne({
+			_user: req.user.id,
+			_id: req.params.id,
+		});
 
-    res.send(blog);
-  });
+		res.send(blog);
+	});
 
-  app.get('/api/blogs', requireLogin, async (req, res) => {
-    // const redis = require('redis');
-    // const redisUrl = 'redis://127.0.0.1:6379';
-    // const client = redis.createClient(redisUrl);
-    // const util = require('util');
-    // client.get = util.promisify(client.get);
-    // // do we have any cached data in redis related to this query
-    // const cachedBlogs = await client.get(req.user.id);
+	app.get('/api/blogs', requireLogin, async (req, res) => {
+		// const redis = require('redis');
+		// const redisUrl = 'redis://127.0.0.1:6379';
+		// const client = redis.createClient(redisUrl);
+		// const util = require('util');
+		// client.get = util.promisify(client.get);
+		// // do we have any cached data in redis related to this query
+		// const cachedBlogs = await client.get(req.user.id);
 
-    // // if yes, then respond to thre request right way and return
-    // if(cachedBlogs) {
-    //   console.log('serving from cache')
-    //   return res.send(JSON.parse(cachedBlogs));
-    // }
-    // // if no, we need to respond to request and update our cache to store the data
-    
-    // const blogs = await Blog.find({ _user: req.user.id });
-    // console.log('serving from mongodb')
-    // res.send(blogs);
-    // client.set(req.user.id, JSON.stringify(blogs));
+		// // if yes, then respond to thre request right way and return
+		// if(cachedBlogs) {
+		//   console.log('serving from cache')
+		//   return res.send(JSON.parse(cachedBlogs));
+		// }
+		// // if no, we need to respond to request and update our cache to store the data
 
-    const blogs = await Blog.find({ _user: req.user.id });
+		// const blogs = await Blog.find({ _user: req.user.id });
+		// console.log('serving from mongodb')
+		// res.send(blogs);
+		// client.set(req.user.id, JSON.stringify(blogs));
 
-    res.send(blogs);
-  });
+		const blogs = await Blog.find({ _user: req.user.id }).cache({
+			key: req.user.id,
+		});
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
-    const { title, content } = req.body;
+		res.send(blogs);
+	});
 
-    const blog = new Blog({
-      title,
-      content,
-      _user: req.user.id
-    });
+	app.post('/api/blogs', requireLogin, async (req, res) => {
+		const { title, content } = req.body;
 
-    try {
-      await blog.save();
-      res.send(blog);
-    } catch (err) {
-      res.send(400, err);
-    }
-  });
+		const blog = new Blog({
+			title,
+			content,
+			_user: req.user.id,
+		});
+
+		try {
+			await blog.save();
+			res.send(blog);
+		} catch (err) {
+			res.send(400, err);
+		}
+	});
 };
--- services/cache.js
+++ services/cache.js
@@ -4,35 +4,35 @@ const util = require('util');
 
 const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
-client.get = util.promisify(client.get);  
+client.hget = util.promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.cache = function () {
-    this.useCache = true;
-    return this;
-}
+mongoose.Query.prototype.cache = function(options = {}) {
+	this.useCache = true;
+	this.hashKey = JSON.stringify(options.key || '');
+	return this;
+};
 
-mongoose.Query.prototype.exec = async function () {
-    if(!this.useCache) {
-        return exec.apply(this, arguments);
-    }
+mongoose.Query.prototype.exec = async function() {
+	if (!this.useCache) {
+		return exec.apply(this, arguments);
+	}
 
-    console.log('Im about to run a query');
-   
-   const key = JSON.stringify(Object.assign({}, this.getQuery(), {
-    collection: this.mongooseCollection.name
-    })
-); 
-    const cacheValue = await client.get(key);
+	console.log('Im about to run a query');
 
-    if(cacheValue) {
-      const doc = JSON.parse(cacheValue);
-       return Array.isArray(doc)  
-        ? doc.map(d => this.model(d))
-        : new this.model(doc);
-    }
+	const key = JSON.stringify(
+		Object.assign({}, this.getQuery(), {
+			collection: this.mongooseCollection.name,
+		})
+	);
+	const cacheValue = await client.hget(this.kahsKey, key);
 
-    const result = await exec.apply(this, arguments);
-    client.set(key, JSON.stringify(result));
-    return result;
-}
+	if (cacheValue) {
+		const doc = JSON.parse(cacheValue);
+		return Array.isArray(doc) ? doc.map(d => this.model(d)) : new this.model(doc);
+	}
+
+	const result = await exec.apply(this, arguments);
+	client.hset(this.hashkey, key, JSON.stringify(result), 'EX', 10);
+	return result;
+};
